ddf7afe1318faf2f5f922d1dee7be586acdbf5c2
Russell S. Lebo <russell@dataink.io>
2024-05-28 18:32:25 -0400

    Format project descriptions to HTML and expand references to images

--- eleventy.config.js
+++ eleventy.config.js
@@ -12,6 +12,55 @@ module.exports = function (eleventyConfig) {
   });
 
   /**
+   * Format content from Airtable into proper HTML
+   */
+  eleventyConfig.addFilter("format", function (str) {
+    var formatted = str;
+    if (str) {
+      formatted = str.split("\n");
+      formatted = "<p>" + formatted.join("</p><p>") + "</p>";
+    }
+    return formatted;
+  });
+
+  /**
+   * Insert referenced content into text.
+   * str: the string containing references to external content; uses format {type, id}
+   * ds: the datasource containing the referenced content; yes, this is far from ideal and
+   * should probably access the datasource directly but I can't figure out how to do that
+   * because promises.
+   */
+  eleventyConfig.addFilter("expand", function (str, ds) {
+    // console.log(str);
+    var expanded = str;
+    if (str) {
+      var results = str.matchAll("{(.*?)}");
+      for (result of results) {
+        var item = result[1].split(",");
+        switch (item[0]) {
+          case "image":
+            // Let's find ourselves an image...
+            var image = ds.find((img) => img["id"] == item[1].trim());
+            expanded = expanded.replace("{" + result[1] + "}", figure(image));
+            break;
+          default:
+          // do nothing
+        }
+      }
+    }
+    return expanded;
+  });
+
+  function figure(image) {
+    var result = "";
+    result += "<figure id='" + image.id + "'>";
+    result += image.pictureElement;
+    result += "<figcaption>" + image.Description + "</figcaption>";
+    result += "</figure>";
+    return result;
+  }
+
+  /**
    * Return a specific item from a given array
    */
   eleventyConfig.addFilter("find", function (arr, attr, values) {
