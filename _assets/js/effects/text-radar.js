07db7331000b66a3000c0a663eea5462392a48b7
Russell S. Lebo <datainkio@users.noreply.github.com>
2024-10-09 16:46:40 +0000

    Apply a radar-type effect to the text of a given container element. Include controls for adjusting real-time.

--- _assets/js/effects/text-radar.js
+++ _assets/js/effects/text-radar.js
@@ -1,24 +1,95 @@
-export function textRadar(id) {
-    const tl = gsap.timeline();
-    const DUPLICATES = 6;
-    const DUR = 1;
-    const Y_DELTA = 6;
-    try {
-        // Start by duplicating the element
-        const source = document.getElementById(id);
-        const originalText = source.textContent;
-        source.textContent = '';
-        for (let i = 0; i < DUPLICATES; i++) {
-            // Duplicate the element
-            const dupe = document.createElement("span");
-            dupe.id = id + "_" + i;
-            dupe.textContent = originalText;
-            dupe.classList.add("text-radar-dupe");
-            source.appendChild(dupe);
-        }
+const DUR = 2;
+const STEPS = 30; // Number of steps in the progression
+const TL = gsap.timeline(); // The timeline we'll return for integration into other sequences
+var source; // The element supplying the text value
+var text; // The text value to repeat
+var amount = 600; // Use settings() to tweak until you find the ideal
+var base = 0 - amount - 1;
 
-    } catch(e) {
-        console.log(e);
+export function textRadar(id) {
+    // settings();
+    populate(id);
+    updateView();
+    TL.pause();
+    return TL;
+};
+
+function populate(id) {
+    source = document.getElementById(id);
+    text = source.textContent;
+    // source.textContent = '';
+    // Create the duplicates
+    for (let i = 0; i <= STEPS; i++) {
+        // Create the span to hold this instance of the original text
+        const dupe = document.createElement("span");
+        // Give it a unique ID
+        dupe.id = id + "_" + i;
+        // Give it the text
+        dupe.textContent = text;
+        // Add to the source container
+        source.appendChild(dupe);
+        dupe.style.zIndex = i;
+        // Style all but the last one to maintain the original
+        if (i < STEPS) {
+            dupe.classList.add("text-radar-dupe");
+            dupe.style.opacity = 1 - i/STEPS * .75;
+        }
     }
-    return tl;
+}
+
+function updateView() {
+    // Define how the duplicates will get distributed on the page
+    let pos = gsap.utils.distribute({
+        base: base,
+        amount: amount,
+        ease: "power2.out",
+        from: "end"
+    }); 
+    let alpha = gsap.utils.distribute({
+        base: 0,
+        amount: 1,
+        ease: "expoScale",
+        from: "end"
+    });
+    TL.add(
+        gsap.to(".text-radar-dupe", {
+        duration: DUR,
+        y: pos,
+        opacity: alpha,
+        ease: "power1.inOut"
+    }));
+}
+
+function settings() {
+    
+    // Create the container
+    const container = document.createElement("div");
+    container.classList.add("absolute", "top-0", "right-0", "w-80", "z-50", "flex");
+    
+    // Create the range slider
+    const control = document.createElement('input');
+    control.type = "range";
+    control.min = 0;
+    control.max = 1000;
+    control.value = amount;
+    control.classList.add("range", "range-primary");
+
+    // Create the value display
+    const display = document.createElement("span");
+    display.classList.add("basis-1/6");
+    display.textContent = control.value;
+
+    container.appendChild(display);
+    container.appendChild(control);
+    document.body.appendChild(container);
+
+    // Update the view when the range changes
+    control.addEventListener('input', (event) => {
+      display.textContent = event.target.value;
+      amount = event.target.value;
+      base = 0 - amount - 1;
+      updateView();
+    });
+
+
 };
