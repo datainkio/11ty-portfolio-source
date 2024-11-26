0f4da3c6f07b315a4fe5aca32e0e0ff2189d67d6
Russell S. Lebo <russell@dataink.io>
2024-11-26 17:00:04 -0500

    Apply PrinterMarks to intro view on home. Add stats, text, awards, and organizations to intro.

--- _assets/js/choreography/Director.js
+++ _assets/js/choreography/Director.js
@@ -8,7 +8,7 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 window.onload = function() {
 
     const SM = new StageManager(document.getElementById("page-content"));
-    SM.video = "/assets/video/Zabiela Background Composite.mp4";
+    SM.video = "/assets/video/Robert Lougheed.mp4";
     SM.gels = ["primary"];
 
     // Initialize ScrollSmoother for smooth scrolling
@@ -27,53 +27,9 @@ window.onload = function() {
     const MAIN = gsap.timeline();
     const HERO = gsap.timeline(); // {yoyo: true, repeat: -1}
 
+
     const landingTL = initLandingView([header, SM.getGel("primary")]);
     const introTL = initIntroView(intro, header);
-
-    PrinterMarks.addTrim(intro, 12);
-    PrinterMarks.addMargins(intro, 16);
-    PrinterMarks.addRegistrationBar(intro, 4);
-    PrinterMarks.addBleed(intro, 8);
-    // PrinterMarks.addStarTarget(intro, 4);
-    // const projectsTL = initProjectsView();
-    /**
-    const testTL = gsap.timeline({
-        scrollTrigger: {
-                trigger: "#blocklines",
-                start: "top top",
-                pin: true
-            }
-    });
-    **/
-
-    // testTL.add(SM.blockline._timeline);
-
-    // 20 YEARS
-
-    // const twentyTL = TextParty.gel(twenty_yrs, gel_params);
-    // twentyTL.pause();
-
-    // INTRO
-
-    
-
-
-
- 
-    /**
-     * The trick for sequencing is to reference specific elements created by other animations rather than
-     * trying to return to baseline each time
-     */
-    // HERO.add(TextParty.gel(h1, Config.WGParams));
-    // HERO.add(TextParty.radar(h1, Config.RadarParams));
-    // HERO.add(TextParty.gel(h1, Config.WGParams));
-    // HERO.add(BlockLine(blockline_container, Config.BlockLineParams), ">");
-    // HERO.add(TextParty.roll(document.getElementById(Config.RadarParams.id + "_0"), Config.TRollParams), ">");
-
-    // BackgroundVideo("bg-video-container", "/assets/video/ICA Mediatheque.mp4");
-
-    // StageManager.init(document.getElementById("page-content"));
-    // StageManager.update("update stage!");
       
 };
 
@@ -97,7 +53,7 @@ function initLandingView(arr) {
     return gsap.to(arr, {
         scrollTrigger: {
             trigger: "#main-header",
-            start: "top top-=75px",
+            start: "top top-=25px",
             end: "bottom 100px",
             scrub: true,
         },
@@ -109,30 +65,28 @@ function initLandingView(arr) {
 }
 
 function initIntroView(elem, trigger) {
-    console.log("initIntroView()", document.getElementById("twenty-years").getElementsByClassName("stat-value")[0]);
-    const gel_params = {
-        id: "wg",
-        paused: true,
-        w: -6,
-        h: -6,
-        range: 1,
-        envelope: 0,
-        duration: 2, 
-        colors: ["text-primary-300", "text-accent-300"], 
-    };
 
-    // 1. Prepare the view to move in
-    gsap.set(elem, {x: 300, y: "+=" + 200, rotation: -10, transformOrigin: "top 75%"});
+    // Apply decorations
+    PrinterMarks.addTrim(intro, 12);
+    PrinterMarks.addMargins(intro, 16);
+    PrinterMarks.addRegistrationBar(intro, 4);
+    PrinterMarks.addBleed(intro, 8);
+
+    // Create the timeline w/scroll trigger
     const result = gsap.timeline({
         id: elem,
         scrollTrigger: {
            trigger: trigger,
-            start: "top bottom",
-            stop: "top center",
+            start: "bottom bottom",
+            end: "bottom top-=300px",
             scrub: true,
         },
     });
-    // 2. Bring in the view
+
+    // 1. Prepare the view to move in
+    gsap.set(elem, {x: "100vw", rotation: -40, transformOrigin: "top 25%"});
+
+    // 2. Bring in the page
     result.add(gsap.to(elem, {
         rotation: 0,
         x: 0,
@@ -140,8 +94,8 @@ function initIntroView(elem, trigger) {
         ease: "sine.inOut",
         // onComplete: twentyTL.play()
     }));
-    // 3. Apply the WanderingGel effect to the "20" text
-    result.add(TextParty.gel(document.getElementById("twenty-years").getElementsByClassName("stat-value")[0], gel_params), ">");
+
+
     return result;
 }
 
